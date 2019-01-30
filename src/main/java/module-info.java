70e6cadc1739c071e0c7131ed0f2565ec4ee8f2f
Jerred Shepherd <ShepherdJerred@gmail.com>
2019-01-30 01:48:47 -0600

    Set SCM info, add build plugins, remove repository and compiler properties

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module com.shepherdjererd.capstone.storage {
   requires com.zaxxer.hikari;
-  requires sqlite.jdbc;
   requires java.sql;
   requires org.apache.logging.log4j;
   requires static lombok;
